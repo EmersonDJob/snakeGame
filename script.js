3ea59bb2b3a1f7587891476fea99c158e717f49d
EmersonDjob <jobbr70@gmail.com>
2021-07-03 07:51:44 -0300

    criação da cobrinha

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 let canvas = document.querySelector('#tutorial')
-let ctx = canvas.getContext('2d')
+let context = canvas.getContext('2d')
 let box = 32
+let snake = [0]
+snake[0] = {
+    x: 8 * box
+    y: 8 * box
+}
 
 function criarBG() {
-    ctx.fillStyle = "green"
-    ctx.fillRect( 0,0, 16 * box, 16 * box)
+    context.fillStyle = "green"
+    context.fillRect( 0,0, 16 * box, 16 * box)
+}
+function criarCobrinha() {
+    for (let i = 0; i < snake.length; i++) {
+        context.fillStyle = "green"
+        context.fillRect(snake[i].x, snake[i].y, box, box)        
+    }
 }
 criarBG()
+criarCobrinha()
