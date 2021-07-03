a0a0bda37f9d461f735a29e39a8f1408df960fcc
EmersonDjob <jobbr70@gmail.com>
2021-07-03 09:46:43 -0300

    inici do movimento

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ let context = canvas.getContext('2d')
 let box = 32
 let snake = [0]
 snake[0] = {
-    x: 8 * box
+    x: 8 * box,
     y: 8 * box
 }
+let direction = "right"
 
 function criarBG() {
     context.fillStyle = "green"
@@ -13,9 +14,27 @@ function criarBG() {
 }
 function criarCobrinha() {
     for (let i = 0; i < snake.length; i++) {
-        context.fillStyle = "green"
+        context.fillStyle = "red"
         context.fillRect(snake[i].x, snake[i].y, box, box)        
     }
 }
-criarBG()
-criarCobrinha()
+function iniciarJogo() {
+
+    criarBG()
+    criarCobrinha()
+    let snakeX = snake[0].x
+    let snakeY = snake[0].y
+    if (direction == "right") snakeX += box
+    if (direction == "left") snakeX -= box
+    if (direction == "up") snakeY -+ box
+    if (direction == "down") snakeY += box
+
+    snake.pop()
+
+    let newHeade = {
+        x : snakeX,
+        y : snakeY
+    }
+    snake.unshift(newHeade)
+}
+let jogo = setInterval(iniciarJogo, 100)
